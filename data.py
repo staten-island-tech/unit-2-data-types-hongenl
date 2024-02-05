6d1f972b0f5b060d76b3bcb46c0c4188e9d1b45f
hongenl <hongenl@nycstudents.net>
2024-02-05 12:36:18 -0500

    ewofkewo

---
+++ data.py
@@ -0,0 +1,46 @@
+""" #Data types 
+#Numbers 1,2,3
+def add(x,y):
+    print(x + y)
+add(1,2)
+#strings "a,b,c"
+name = "Mark"
+def greeting(person):
+    print(person)
+greeting(name)
+#1 & "1" are not the same
+add("1","2")
+#undefined/null
+
+#booleans
+tenure = False
+def is_tenured(status):
+    if(status == True):
+        print("they have tenure")
+    else:
+        print("they are not tenure") """
+
+""" x = 3
+y = float(3)
+print(x,y) """
+
+""" values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i)
+
+print(values[0])
+print(values[6])
+
+x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z) """
+
+sentence = "I love pizza and icecream"
+word = sentence.split( )
+z = 1
+for i in sentence:
+    print(z)
+
---
+++ lists.py
@@ -0,0 +1,15 @@
+animals = ["Zebra", "Camel", "Ape"]
+print(animals[0])
+
+for animal in animals:
+        print(animal)
+
+for animal in animals:
+    if animal == ("Camel"):
+        print("We're in the desert")
+
+#Strings operate like lists
+x = "Hello"
+#print(x[0])
+
+
