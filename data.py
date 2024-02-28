45e47f70f01105959d242b25c1e075eb2285695f
hongenl <hongenl@nycstudents.net>
2024-02-28 12:35:41 -0500

    Update data.py

--- data.py
+++ data.py
@@ -87,10 +87,16 @@ for i in range(1,number+1):
     if number%i == 0:
         print(i) """
 
-""" number1 = int(input("Put your first number here:"))
+number1 = int(input("Put your first number here:"))
 number2 = int(input("Put your second number here:"))
 
-for i in range(1,number1+1):
-    if(number1 % i == 0) and (number2 % i == 0):
+def gcf(x,y):
+    if x > y:
+        smaller = y
+    else:
+        smaller = x
+    for i in range(1, smaller+1):
+        if(x % i == 0) and (y % i == 0):
             gcf = i
-print(gcf) """
+    return(gcf)
+print(gcf(number1,number2))
